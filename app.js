eac1be34dd84a2b18d42d787c07e7fb6e37318be
Abimbola Olaitan <craftwordltd@gmail.com>
2018-03-19 08:34:24 -0700

    modify files

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const JSONbig = require('json-bigint');
 const async = require('async');
 
 const REST_PORT = (process.env.PORT || 5000);
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_ACCESS_TOKEN = '2bd59f7424bc4440a735229ab6c68b89';
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
-const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+const FB_VERIFY_TOKEN = 'test_token';
+const FB_PAGE_ACCESS_TOKEN = 'EAACeJX7kxMcBAIcmyNYfqTRoyZBZBlYwYcqbHCl6MNjugIykz5nXU1oL7kO3fsxKWbAU2Qyp04B4HRJqeComlymR8y1UY49hZARQ9a0zGbVKCJMjooIOxI1jZCr0BEt5qQdIhR35AvUmoo0jbaXwZCSXPR7t3PyrClKeZBbXwrdAZDZD';
 const FB_TEXT_LIMIT = 640;
-const NEWS_API = process.env.NEWS_API;
+const NEWS_API = 'f7fb2fb15b24480ba39b5795c846ed59';
 
 const FACEBOOK_LOCATION = "FACEBOOK_LOCATION";
 const FACEBOOK_WELCOME = "FACEBOOK_WELCOME";
@@ -620,7 +620,7 @@ app.post('/news', (req, res) => {
     return elements;
 
  }
- 
+
 
 app.listen(REST_PORT, () => {
     console.log('Rest service ready on port ' + REST_PORT);
